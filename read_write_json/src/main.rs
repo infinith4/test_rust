5789d5fe2d443a3ff591b9f59116f1925b24ef84
infinith4 <infinith4@gmail.com>
2022-08-21 15:25:43 +0900

    mod

---
+++ read_write_json/src/main.rs
@@ -0,0 +1,53 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Memo {
+    id: isize,
+    body: String,
+    star: bool,
+}
+
+type Memos = HashMap<String, Memo>;
+
+fn main() {
+    write_file("hello".to_string());
+    read_file();
+}
+
+fn write_file(body: String) -> std::io::Result<()> {
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open("memo.json")?;
+    println!("file: {:#?}", file);
+
+    let memo = Memo {
+        id: 100,
+        body: body,
+        star: true,
+    };
+
+    let mut memos = HashMap::new();
+    memos.insert(memo.id.to_string(), memo);
+
+    let memos_text = serde_json::to_string(&memos).unwrap();
+    println!("memos: {}", &memos_text);
+    write!(&file, "{}", memos_text)?;
+
+    Ok(())
+}
+
+fn read_file() -> std::io::Result<()> {
+    let mut memos_file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open("memo.json")?;
+    let reader = BufReader::new(memos_file);
+    let memos: Memos = serde_json::from_reader(reader)?;
+    println!("memo_text: {:?}", memos);
+    Ok(())
+}
