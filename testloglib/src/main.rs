2be5c86d1584798304ce75de045264a3f17ed9b6
infinith4 <infinith4@gmail.com>
2022-10-30 15:58:24 +0900

    output log

--- testloglib/src/main.rs
+++ testloglib/src/main.rs
@@ -1,10 +1,17 @@
 mod logLib;
+use chrono::{DateTime, Local};
+use log::error;
+use std::fmt;
 
 fn main() {
     println!("Hello, world!");
-    let file_name: &str = "test20221028.log";
-    let logLib = LogLib::new(file_name);
-    // error!("Bright red error");
+    let local_datetime = Local::now().to_rfc3339().to_string();
+    let local_datetime_str: String = String::from(local_datetime);
+    let local_datetime_str2: &str = &local_datetime_str;
+    //println!("aaaa{}", str2);
+    let file_name = format!("{}.log", local_datetime_str2);
+    let logLib = logLib::LogLib::new(&file_name);
+    error!("Bright red error");
     // info!("This only appears in the log file");
     // debug!("This level is currently not enabled for any logger");
 }
