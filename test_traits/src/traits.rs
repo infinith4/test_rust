bc155443142d34e750316fe97cd543f4b8544a8c
infinith4 <infinith4@gmail.com>
2022-12-25 20:31:41 +0900

    Update traits.rs

--- test_traits/src/traits.rs
+++ test_traits/src/traits.rs
@@ -23,8 +23,46 @@ pub fn run() {
     let banana = Banana {};
     get_price(apple);
     get_price(banana);
+    let tweet = Tweet {
+        username: String::from("horse_ebook"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
 }
 
 fn get_price<T: Fruits>(fruits: T) {
     println!("price is: {}", fruits.price());
+}
+
+trait Summary {
+    fn summarize(&self) -> String;
+}
+
+struct NewsArticle {
+    headline: String,
+    location: String,
+    author: String,
+    content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+struct Tweet {
+    username: String,
+    content: String,
+    reply: bool,
+    retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
 }
